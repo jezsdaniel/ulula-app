ae63b5c444bb75cf1c88b632de779c89c800c432
jezsdaniel <jadendaniel7@gmail.com>
2023-01-09 01:53:49 -0500

    Add text styles

--- lib/core/constants/colors.dart
+++ lib/core/constants/colors.dart
@@ -9,5 +9,4 @@ class AppColors {
   static const color4 = Color(0xffACB9D2);
   static const color5 = Color(0xffDEE3ED);
   static const color6 = Color(0xffFFFFFF);
-
 }
---
+++ lib/core/constants/text_styles.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import 'package:ulula/core/constants/colors.dart';
+
+class AppTextStyles {
+  AppTextStyles._();
+
+  static const TextStyle bold20 = TextStyle(
+    fontSize: 20,
+    height: 1.35,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'OpenSans',
+    color: AppColors.color2,
+  );
+
+  static const TextStyle semiBold16 = TextStyle(
+    fontSize: 16,
+    height: 1.35,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'OpenSans',
+    color: AppColors.color2,
+  );
+
+  static const TextStyle regular16 = TextStyle(
+    fontSize: 16,
+    height: 1.35,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'OpenSans',
+    color: AppColors.color2,
+  );
+
+  static const TextStyle regular14 = TextStyle(
+    fontSize: 14,
+    height: 1.35,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'OpenSans',
+    color: AppColors.color2,
+  );
+
+  static const TextStyle regular12 = TextStyle(
+    fontSize: 12,
+    height: 1.35,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'OpenSans',
+    color: AppColors.color2,
+  );
+}
